6f65aaa19ff4518e1c6bc63602cccc94cd217491
Kim Seungha <seungha.me@gmail.com>
2018-11-29 13:59:15 +0900

    Layout 컴포넌트 추가

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import ProductDetail from '../containers/ProductDetail';
+import Layout from '../components/Layout';
 
 export default class ProductPage extends Component {
   render() {
     const { match } = this.props;
     const productId = match.params.productId;
     return (
-      <div>
+      <Layout>
         <ProductDetail productId={productId} />
-      </div>
+      </Layout>
     );
   }
 }
