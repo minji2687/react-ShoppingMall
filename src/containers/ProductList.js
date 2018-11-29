6399ae2d56098c9fbcdc9f6e535afe14ee35da02
Kim Seungha <seungha.me@gmail.com>
2018-11-29 12:06:09 +0900

    ProductListView에 withLoading 적용

--- src/containers/ProductList.js
+++ src/containers/ProductList.js
@@ -21,12 +21,12 @@ export default class ProductList extends Component {
   }
 
   render() {
-    const { products } = this.state;
+    const { products, loading } = this.state;
     const productsList = products.map(p => ({
       title: p.title,
       id: p.id,
       imgURL: p.mainImgUrl,
     }));
-    return <ProductListView products={productsList} />;
+    return <ProductListView loading={loading} products={productsList} />;
   }
 }
